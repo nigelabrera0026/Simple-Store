c0eea862540abc6e218d0213b5afd7d0e47a1458
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-02-17 23:51:23 -0600

    Finished loading csv file and generated db with data from csv. Created category and reference it to products table. Edited Views for show and index.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   validates :title, :price, :stock_quantity, presence: true
+  belongs_to :category
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,28 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-# Generate fake data with Faker
-676.times do
-  Product.create(
-    title:          Faker::Commerce.product_name,
-    description:    Faker::Lorem.paragraph,
-    price:          Faker::Commerce.price(range: 0..999.9, as_string: false),
-    stock_quantity: Faker::Number.between(from: 0, to: 9999)
+require "csv"
+# Seed products with associated categories
+CSV.foreach(Rails.root.join("db", "products.csv"), headers: true) do |row|
+  # Find or create the category
+  category = Category.find_or_create_by(name: row["category"])
+
+  # Create the product with the associated category
+  category.products.create(
+    title: row['name'],
+    description: row['description'],
+    price: row['price'],
+    stock_quantity: row['stock quantity']
   )
 end
+
+# Generate fake data with Faker
+# require "faker"
+# 676.times do
+#   Product.create(
+#     title:          Faker::Commerce.product_name,
+#     description:    Faker::Lorem.paragraph,
+#     price:          Faker::Commerce.price(range: 0..999.9, as_string: false),
+#     stock_quantity: Faker::Number.between(from: 0, to: 9999)
+#   )
+# end
